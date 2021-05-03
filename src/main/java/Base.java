1f4022058e85092e9211847a2d1a87a2a8f174c3
GafBoy <fear55@list.ru>
2021-05-04 01:49:43 +0300

    Задание №3
    1. Реализовать базовые операции калькулятора (+,-,/,*) для предыдущего задания
    
    2. Поиск максимального элемента в массиве. Для начала задать массив слов.Размерность массива произвольна, задается в консоли. После чего в консоли вводятся слова в количестве равном заданной длине массива. В полученном массиве необходимо найти самое длинное слово. Результат вывести на консоль
    
    3. Программа должна выполнять одно из заданий на выбор. Если в консоли ввести 1 - запуститься выполнение калькулятора, если 2 - поиск максимального слова в массиве.

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -3,10 +3,58 @@ import java.util.Scanner;
 public class Base {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
+        System.out.println("Введите 1(чтобы запустить калькулятор) или 2(чтобы запустить поиск макс. элемента в массиве): ");
+        Integer number = console.nextInt();
+        if (number.equals(1)){
+            System.out.println("Запускаем калькулятор.");
+            calculator();
+        }
+        if (number.equals(2)){
+            System.out.println("Запускаем поиск самого длинного слова");
+            findMax();
+        }
+    }
+
+    private static void calculator() {
+        Scanner console = new Scanner(System.in);
         System.out.print("Введите дробное число: ");
         Float first = console.nextFloat();
+        System.out.print("Введите символ операции (+,-,/,*): ");
+        String operation = console.next();
         System.out.print("Введите дробное число: ");
         Float second = console.nextFloat();
-        System.out.printf( "Сумма= %.4f", first + second);
+        if (operation.equals("+")) {
+            System.out.printf("%.4f + %.4f = %.4f", first, second, first + second);
+        }
+        if (operation.equals("-")) {
+            System.out.printf("%.4f - %.4f = %.4f", first, second, first - second);
+        }
+        if (operation.equals("*")) {
+            System.out.printf("%.4f * %.4f = %.4f", first, second, first * second);
+        }
+        if (operation.equals("/")) {
+            System.out.printf("%.4f / %.4f = %.4f", first, second, first / second);
+        }
+    }
+    private static void findMax(){
+        Scanner console = new Scanner(System.in);
+        System.out.print("Введите количество слов в массиве: ");
+        Integer n = console.nextInt();
+        String arr[] = new String[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Введите слово " + (i+1) + ":" );
+            arr[i] = console.next();
+        }
+        Integer max = -1;
+        String maxWord = " ";
+        for (int i = 0; i < n; i++) {
+           String word = arr[i];
+            if (word.length() > max) {
+                max = word.length();
+                maxWord = word;
+
+            }
+        }
+        System.out.println("Самое длинное слово в массиве: " + maxWord);
     }
 }
